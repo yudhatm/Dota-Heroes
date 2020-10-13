59ea2ff1203fb228a041ff46a3c6eaa2791cbc29
Prabaesa Yudha Tama <prabaesayudhatama@gmail.com>
2020-10-13 15:58:12 +0700

    Initial Commit

---
+++ Dota Heroes ListTests/Dota_Heroes_ListTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dota_Heroes_ListTests.swift
+//  Dota Heroes ListTests
+//
+//  Created by Yudha on 13/10/20.
+//  Copyright © 2020 Yudha. All rights reserved.
+//
+
+import XCTest
+@testable import Dota_Heroes_List
+
+class Dota_Heroes_ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
